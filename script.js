0f29ca8ddc9b215161c01be789bbae5c6dd8fbea
An Wangzhe <Wangzhe.An@andritz.com>
2025-12-02 16:31:06 +0800

    Initial commit: responsive web app

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.getElementById('clickBtn').addEventListener('click', () => {
   const count = localStorage.getItem('count') || 0;
   const newCount = parseInt(count) + 1;
   localStorage.setItem('count', newCount);
-  document.getElementById('result').innerText = `你点了 ${newCount} 次！`;
+  document.getElementById('result').innerText = `你同意了 ${newCount} 次！`;
 });
